b12f7976ac202dd4a99ca2f9c9d878d95bea14c0
HEllicott <hannah.ellicott@os.uk>
2020-03-04 11:36:06 +0000

    added Generics example code

---
+++ Generics/Generics/CycList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class CycList<T> where T : IDisposable
+    {
+        private List<T> _items = new List<T>();
+        private int _pointer = 0;
+        private int _size;
+
+        public CycList(int size)
+        {
+
+            this._size = size;
+            for (int i = 0; i < this._size; i++)
+            {
+                this._items.Add(default(T));
+            }
+        }
+
+        public void Add(T item)
+        {
+            Console.WriteLine($"Adding {item} at position {this._pointer}");
+            this._items[this._pointer] = item;
+            this._pointer++;
+            if (this._pointer >= this._size)
+            {
+                this._pointer = 0;
+            }
+        }
+
+        public T GetItem(int index)
+        {
+            Console.WriteLine($"Getting item from position {index}");
+            if ((index > 0) && (index < this._size))
+            {
+                return this._items[index];
+            }
+            else return default(T);
+        }
+
+        public void View()
+        {
+            int itemsPrinted = 0;
+            int tempPointer = this._pointer;
+            while (itemsPrinted < this._size)
+            {
+                Console.WriteLine($"Pos{tempPointer}:{this._items[tempPointer]}");
+                itemsPrinted++;
+                tempPointer++;
+                if (tempPointer >= this._size)
+                {
+                    tempPointer = 0;
+                }
+
+            }
+            Console.WriteLine("--------------");
+        }
+
+
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CycList<int> ints = new CycList<int>(5);
+            ints.Add(7);
+            ints.Add(36);
+            ints.View();
+            Console.WriteLine(ints.GetItem(1));
+            ints.Add(3);
+            ints.Add(2);
+            ints.Add(1);
+            ints.Add(100);
+            ints.View();
+
+            Console.WriteLine("Hello World!");
+            CycList<string> strs = new CycList<string>(5);
+            strs.Add("1");
+            strs.Add("2");
+            strs.View();
+            Console.WriteLine(strs.GetItem(1));
+            strs.Add("3");
+            strs.Add("4");
+            strs.Add("5");
+            strs.Add("100");
+            strs.View();
+        }
+    }
+}

4cd4e66bbbd3e30a9331ffb5a543c785fad99cfa
HEllicott <hannah.ellicott@os.uk>
2020-03-04 14:34:49 +0000

    added example extension methods

---
+++ ExtensionMethods/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BankSystem
+{
+    public static class MyExtensions
+    {
+        public static bool InRange(this double num, double min, double max)
+        {
+            return ((num > min) && (num < max));
+        }
+
+        public static string PasswordStrength(this string password)
+        {
+            bool length = password.Length >= 8;
+            bool digit = false;
+            bool nonAlphanum = false;
+
+            foreach (char charac in password)
+            {
+                if (char.IsDigit(charac))
+                {
+                    digit = true;
+                }
+                else if (!char.IsLetterOrDigit(charac))
+                {
+                    nonAlphanum = true;
+                }
+            }
+            if (length && digit && nonAlphanum)
+            {
+                return "strong";
+            }
+            else
+            {
+                return "weak;";
+            }
+        }
+    }
+}

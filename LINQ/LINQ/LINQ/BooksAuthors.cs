124dbb350831e9bff2d7c0bc51583f24d98f8f0e
HEllicott <hannah.ellicott@os.uk>
2020-03-05 10:00:31 +0000

    Linq examples

---
+++ LINQ/LINQ/LINQ/BooksAuthors.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+
+        public Book(string title, string author, double price)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Price = price;
+        }
+    }
+
+    class Author
+    {
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+
+        public Author(string name, int birthYear)
+        {
+            this.Name = name;
+            this.BirthYear = birthYear;
+        }
+    }
+}
--- LINQ/LINQ/LINQ/Program.cs
+++ LINQ/LINQ/LINQ/Program.cs
@@ -8,11 +8,48 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+
+            simpleCollectionExamples();
+            objectCollectionExamples();
+            joinsExamples();
+        }
+
+        static void joinsExamples()
+        {
+            Book[] books = { new Book("The BFG", "Whats his name", 12.99), new Book("The Bible", "12 disciples", 20.34) };
+            Author[] authors = { new Author("Whats his name", 1947), new Author("12 disciples", 1) };
+
+            var inner = from b in books
+                        join a in authors on b.Author equals a.Name
+                        select new { a.Name, a.BirthYear, b.Title };
+            Print("Inner join", inner);
+        }
+
+        static void objectCollectionExamples()
+        {
+            // working with object collections
+            BankAccount[] accounts = { new BankAccount("Sammy", 12.98),
+                                       new BankAccount("Harry", 170.13),
+                                       new BankAccount("Thomas", 55.70) };
+
+            var allAccs = from a in accounts
+                          select a;
+            Print("All accounts", allAccs);
+
+            // accounts with balance above 20
+            var accs20 = from a in accounts
+                         where a.Balance > 20
+                         select a.AccountHolder;
+            Print("Accounts above £20", accs20);
+        }
+
+        static void simpleCollectionExamples()
+        {
             int[] lotteryNums = { 36, 66, 12, 6, 21, 3 };
             Print("original nums", lotteryNums);
             // output all nums
             var allNums = from n in lotteryNums
-                       select n;
+                          select n;
             Print("All nums", allNums);
 
             // ascending order
@@ -38,12 +75,12 @@ namespace LINQ
             Console.WriteLine($"SUM: {sum}");
         }
 
-        static void Print(string label, IEnumerable<int> nums)
+        static void Print<T>(string label, IEnumerable<T> items)
         {
             Console.WriteLine($"-{label}-");
-            foreach (var num in nums)
+            foreach (var item in items)
             {
-                Console.WriteLine(num);
+                Console.WriteLine(item);
             }
             Console.WriteLine("------");
         }
